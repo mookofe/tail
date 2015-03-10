5952c0f49895161102b19d805c43355f3c29f1af
Victor Cruz <cruzrosario@gmail.com>
2015-03-10 15:44:16 -0400

    Configuration file added

---
+++ config/tail.php
@@ -0,0 +1,36 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default AMQP Server Connection
+    |--------------------------------------------------------------------------
+    |
+    | The name of your default AMQP server connection. This connection will 
+    | be used as the default for all queues operations unless a different 
+    | name is given when performing said operation. This connection name
+    | should be listed in the array of connections below.
+    |
+    */
+    'default' => 'default_connection',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Queues Connections
+    |--------------------------------------------------------------------------
+    */
+
+    'connections' => array(
+
+        'default_connection' => array(
+            'host'         => 'localhost',
+            'port'         => 5672,
+            'username'     => 'guest',
+            'password'     => 'guest',
+            'vhost'        => '/',
+            'exchange'     => 'amq.direct',
+            'consumer_tag' => 'consumer',
+        ),      
+    ),
+);
