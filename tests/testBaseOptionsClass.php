84378fc6e0e77b75509ea23b77478f4b496b23ea
Victor Cruz <cruzrosario@gmail.com>
2015-07-05 22:24:40 -0400

    First test

---
+++ tests/testBaseOptionsClass.php
@@ -0,0 +1,19 @@
+<?php namespace Mookofe\Tail\Test;
+ 
+use Mookofe\Tail\Tail;
+
+/**
+ * Test base option class
+ *
+ * @author Victor Cruz <cruzrosario@gmail.com> 
+ */
+class testBaseOptionsClass extends \PHPUnit_Framework_TestCase
+{
+    
+
+    public function testAddWithoutOptions()
+    {
+        
+        //$tail->add('test-queue', "Message sent at :" . time());
+    }
+}
