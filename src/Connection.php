4d20d66f211e04b18a60e9cb9e83cc1b47f0e75e
Michal Kierat <mkierat@rmn.com>
2016-08-17 13:26:45 +0100

    Allow configuring heartbeat, keepalive, read_write_timeout, connection_timeout as well as ssl context options

--- src/Connection.php
+++ src/Connection.php
@@ -2,7 +2,7 @@
 
 use Exception;
 use Mookofe\Tail\BaseOptions;
-use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Connection\AMQPSSLConnection;
 
 /**
  * Connection class, used to manage connection to the RabbitMQ Server
@@ -47,6 +47,41 @@ class Connection extends BaseOptions{
     public $consumer_tag;
 
     /**
+     * RabbitMQ connection SSL context options
+     *
+     * @var array
+     */
+    public $ssl_context_options;
+
+    /**
+     * RabbitMQ connection timeout in seconds
+     *
+     * @var float
+     */
+    public $connection_timeout;
+
+    /**
+     * RabbitMQ connection read/write timeout in seconds
+     *
+     * @var float
+     */
+    public $read_write_timeout;
+
+    /**
+     * RabbitMQ connection keepalive flag
+     *
+     * @var bool
+     */
+    public $keepalive;
+
+    /**
+     * RabbitMQ connection heartbeat in seconds
+     *
+     * @var int
+     */
+    public $heartbeat;
+
+    /**
      * RabbitMQ AMQP Connection
      *
      * @var PhpAmqpLib\Connection\AMQPConnection
@@ -69,7 +104,19 @@ class Connection extends BaseOptions{
      */
     public function __construct(array $options = null)
     {
-        $this->allowedOptions = array_merge($this->allowedOptions, array('host', 'port', 'username', 'password', 'consumer_tag'));
+        $this->allowedOptions = array_merge($this->allowedOptions, array(
+                'host',
+                'port',
+                'username',
+                'password',
+                'consumer_tag',
+                'ssl_context_options',
+                'connection_timeout',
+                'read_write_timeout',
+                'keepalive',
+                'heartbeat'
+            )
+        );
 
         if (!$options)
             $options = $this->buildConnectionOptions();
@@ -86,7 +133,21 @@ class Connection extends BaseOptions{
     {
         try
         {
-            $this->AMQPConnection = new AMQPConnection($this->host, $this->port, $this->username, $this->password, $this->vhost);
+            $additionalConnectionOptions = array();
+            foreach (array('connection_timeout', 'read_write_timeout', 'keepalive', 'heartbeat') as $option) {
+                if (isset($this->$option)) {
+                    $additionalConnectionOptions[$option] = $this->$option;
+                }
+            }
+            $this->AMQPConnection = new AMQPSSLConnection(
+                $this->host,
+                $this->port,
+                $this->username,
+                $this->password,
+                $this->vhost,
+                $this->ssl_context_options,
+                $additionalConnectionOptions
+            );
             $this->channel = $this->AMQPConnection->channel();
             $this->channel->queue_declare($this->queue_name, false, true, false, false);
             $this->channel->exchange_declare($this->exchange, $this->exchange_type, false, true, false);
