52b2d9d10ad278e378a62b89dbadceb589b51096
Paing Hein Thu <painghein.thu@frontiir.net>
2019-09-09 15:13:22 +0630

    support laravel 6.0

--- tests/testBaseOptions.php
+++ tests/testBaseOptions.php
@@ -2,38 +2,28 @@
 
 use Mockery;
 use Mookofe\Tail\BaseOptions;
+use PHPUnit\Framework\TestCase;
 
 
 /**
  * Test base option class
  *
- * @author Victor Cruz <cruzrosario@gmail.com> 
+ * @author Victor Cruz <cruzrosario@gmail.com>
  */
-class testBaseOptions extends \PHPUnit_Framework_TestCase
+class testBaseOptions extends TestCase
 {
-
-    protected $input;
-    
-    public function __construct()
-    {
-        $this->input = Mockery::mock('Illuminate\Config\Repository');
-        
-    }
-
-    public function tearDown()
-    {
-        Mockery::close();
-    }
-
     public function testValidateOptions()
     {
-        $options = array('queue_name' => 'this_queue');
-        $baseOptions = new BaseOptions($this->input);
+        $input = Mockery::mock('Illuminate\Config\Repository');
 
+        $options = array('queue_name' => 'this_queue');
+        $baseOptions = new BaseOptions($input);
         $result = $baseOptions->validateOptions($options);
 
         //Asserts
         $this->assertInstanceOf('Mookofe\Tail\BaseOptions', $result);
+
+        Mockery::close();
     }
 
     /**
@@ -41,36 +31,45 @@ class testBaseOptions extends \PHPUnit_Framework_TestCase
      */
     public function testValidateOptionsInvalid()
     {
-        $options = array('invalid_field' => 'this_is_invalid_field');
-        $baseOptions = new BaseOptions($this->input);
+        $input = Mockery::mock('Illuminate\Config\Repository');
 
-        $result = $baseOptions->validateOptions($options);        
+        $options = array('invalid_field' => 'this_is_invalid_field');
+        $baseOptions = new BaseOptions($input);
+        $result = $baseOptions->validateOptions($options);
+
+        Mockery::close();
     }
 
     public function testSetOptions()
     {
-        $options = array('queue_name' => 'this_queue');
-        $baseOptions = new BaseOptions($this->input);
+        $input = Mockery::mock('Illuminate\Config\Repository');
 
+        $options = array('queue_name' => 'this_queue');
+        $baseOptions = new BaseOptions($input);
         $baseOptions->setOptions($options);
 
-        //Assertss        
+        //Assertss
         $this->assertObjectHasAttribute('queue_name', $baseOptions);
         $this->assertEquals($baseOptions->queue_name, $options['queue_name']);
+        Mockery::close();
     }
 
     public function testBuildConnectionOptions()
     {
+
+        $input = Mockery::mock('Illuminate\Config\Repository');
         //Mock Input object
-        $this->input->shouldReceive('get')->once()->andReturn('just_to_return');
-        $this->input->shouldReceive('get')->once()->andReturn(array());
-        
+        $input->shouldReceive('get')->once()->andReturn('just_to_return');
+        $input->shouldReceive('get')->once()->andReturn(array());
+
         //Setup enviroment
-        $baseOptions = new BaseOptions($this->input);
+        $baseOptions = new BaseOptions($input);
         $options = $baseOptions->buildConnectionOptions();
 
         //Asserts
         $this->assertInternalType('array', $options);
         $this->assertArrayHasKey('queue_name', $options);
+
+        Mockery::close();
     }
 }
